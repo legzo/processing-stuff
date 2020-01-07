7c608c285bf7b589fdf1c549c9b52de11d97e652
Julien Terrier <julien.terrier@orange.com>
2020-01-07 15:19:10 +0100

    Adds a vehicle chasing the cursor

--- flock/sketch/sketch.ts
+++ flock/sketch/sketch.ts
@@ -1,13 +1,37 @@
 const sketch = (p: p5) => {
 
+    let v: Vehicle;
+
     p.setup = () => {
         // Canvas size
         p.createCanvas(600, 400);
+        v = new Vehicle(p, 300, 200, 5);
+
+        p.fill(255);
+    };
+
+    let mousePosition = function() {
+        return p.createVector(p.mouseX, p.mouseY)
+    };
+
+    let force = function() {
+        return p5.Vector.sub(mousePosition(), v.position)
+            .mult(0.1)
+            .sub(v.velocity)
     };
 
     p.draw = () => {
-        p.fill(255);
-        p.circle(p.width / 2, p.height / 2, 200)
+        p.background(55);
+
+        v.applyForce(force());
+        v.update();
+        v.display();
+    };
+
+    p.mouseClicked = () => {
+        console.log(`Force : ${force()}`);
+        v.position = p.createVector(300, 200);
+        v.velocity = p.createVector(0, 0)
     };
 
 };
---
+++ flock/sketch/vehicle.ts
@@ -0,0 +1,43 @@
+class Vehicle {
+    constructor(
+        readonly p: p5,
+        readonly x: number,
+        readonly y: number,
+        readonly size: number
+    ) {
+        this.position = p.createVector(x, y);
+        this.velocity = p.createVector(0, 0);
+        this.acceleration = p.createVector(0, 0);
+    }
+
+    position: p5.Vector;
+    velocity: p5.Vector;
+    acceleration: p5.Vector;
+
+    applyForce(force: p5.Vector) {
+        this.acceleration.add(force)
+    }
+
+    update() {
+        this.velocity.add(this.acceleration);
+        this.position.add(this.velocity);
+        this.acceleration.mult(0)
+    }
+
+    display() {
+        let theta = this.velocity.heading() + this.p.PI / 2;
+        this.p.fill(127);
+        this.p.stroke(200);
+        this.p.strokeWeight(1);
+        this.p.push();
+        this.p.translate(this.position.x, this.position.y);
+        this.p.rotate(theta);
+        this.p.beginShape();
+        this.p.vertex(0, -this.size * 2);
+        this.p.vertex(-this.size, this.size * 2);
+        this.p.vertex(this.size, this.size * 2);
+        this.p.endShape(this.p.CLOSE);
+        this.p.pop();
+    }
+
+}
