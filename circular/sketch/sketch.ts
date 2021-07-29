adb6e91f543ec00665fc00bd70e741ebe0a116ed
Julien Terrier <julien.terrier@orange.com>
2021-07-29 10:40:37 +0200

    Ajoute p5.gui pour modifier le paramétrage du sketch

--- circular/sketch/sketch.ts
+++ circular/sketch/sketch.ts
@@ -1,67 +1,69 @@
-const sketch = (p: p5) => {
+// noinspection JSPotentiallyInvalidConstructorUsage
+const makeSketch = () => {
 
-    // Canvas size
-    const width = 300
-    const height = 300
-    let side = 50
+    const sketch = (p: p5) => {
 
-    p.setup = () => {
+        let params = {
+            side: 50,
+            definition: 100,
+            definitionMin: 20,
+            definitionMax: 400
+        };
+        let gui: any;
+        let guiVisible = true
 
-        // Modes
-        p.rectMode(p.CENTER)
-        p.colorMode(p.HSB, 100)
 
-        p.createCanvas(width, height)
-        p.background(255)
+        p.setup = () => {
 
-        drawStuff()
-    }
+            // Modes
+            p.rectMode(p.CENTER)
+            p.colorMode(p.HSB, 100)
 
-    p.draw = () => {
+            // @ts-ignore
+            gui = p.createGui(p);
+            gui.addObject(params);
 
-    }
+        }
 
-    function drawStuff() {
-        let corna = 20
-        let cornb = 20
-        p.background(255)
-        for (let i = 0; i < width; i++) {
-            for (let j = 0; j < height; j++) {
-                let x = corna + i * side / 100
-                let y = cornb + j * side / 100
+        p.draw = () => {
+            drawSquare(params.definition, params.side, "large")
+        }
 
-                const intValue = Math.floor(x * x + y * y)
-                
-                if (intValue % 2 == 0) {
-                    p.fill(0)
-                    p.point(i, j)
-                }
-                                
-                if (intValue % 3 == 0) {
-                    p.fill(100)
-                    p.point(i, j)
-                }
-                else {
-                    
+        function drawSquare(definition: number, side: number, cssClass: string) {
+            p.createCanvas(definition, definition).addClass(cssClass)
+            let corna = 20
+            let cornb = 20
+            p.background(255)
+            for (let i = 0; i < definition; i++) {
+                for (let j = 0; j < definition; j++) {
+                    let x = corna + i * side / 100
+                    let y = cornb + j * side / 100
+
+                    const intValue = Math.floor(x * x + y * y)
+
+                    if (intValue % 2 == 0) {
+                        p.fill(0)
+                        p.point(i, j)
+                    }
+
+                    if (intValue % 3 == 0) {
+                        p.fill(100)
+                        p.point(i, j)
+                    }
                 }
             }
         }
-    }
 
-    p.keyPressed = () => {
-        if (p.key === "k") {
-            side++
-            drawStuff()
-        }
-
-        if (p.key === "j") {
-            side--
-            drawStuff()
+        p.keyPressed = () => {
+            if (p.key === 'p') {
+                guiVisible = !guiVisible;
+                if (guiVisible) gui.show(); else gui.hide();
+            }
         }
 
     }
 
+    return new p5(sketch)
 }
 
-new p5(sketch)
-
+makeSketch()
